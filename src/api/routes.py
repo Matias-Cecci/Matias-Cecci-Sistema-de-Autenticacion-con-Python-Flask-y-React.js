9ce7bc129757fc96abc81e204faff80cb6625c87
MATIAS EXEQUIEL CECCI <matias_cecci@hotmail.com>
2023-01-24 19:49:22 +0000

    avanzando en el login

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,9 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
 
 api = Blueprint('api', __name__)
 
@@ -16,3 +19,14 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/login', methods=['POST'])
+def user_login():
+    body_email = request.json.get("email")
+    body_password = request.json.get("password")
+    user = User.query.filter_by(email= body_email, password=body_password).first()
+    if not user:
+        return jsonify({"Error": "Invalid credentials"}), 401
+    token = create_access_token(identity=user.id)
+
+    return jsonify({"response": "Hola"}), 200
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,51 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+  const sendLoginCredential = async () => {
+    const response = await fetch(
+      "https://3001-matiascecci-matiascecci-7s5aq9y5seh.ws-eu83.gitpod.io/api/login",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      }
+    );
+  };
+
+  return (
+    <div className="text-center mt-5">
+      LOGIN
+      <div>
+        <label htmlFor="email">Email</label>
+        <input
+          name="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input
+          name="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
+      </div>
+      <button className="btn btn-primary" onClick={() => sendLoginCredential()}>
+        Login
+      </button>
+    </div>
+  );
 };
